9e11d6c8fa41d0c5451f934a2dd8ad1b6b173fbc
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-10-09 14:06:03 -0300

    Add csv and service

---
+++ report/src/main/java/org/br/matheuscordeiro/service/OpportunityService.java
@@ -0,0 +1,15 @@
+package org.br.matheuscordeiro.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.matheuscordeiro.dto.OpportunityDto;
+import org.br.matheuscordeiro.dto.ProposalDto;
+import org.br.matheuscordeiro.dto.QuotationDto;
+
+import java.util.List;
+
+@ApplicationScoped
+public interface OpportunityService {
+    void buildOpportunity(ProposalDto proposalDto);
+    void saveQuotation(QuotationDto quotationDto);
+    List<OpportunityDto> generateOpportunityDate();
+}
